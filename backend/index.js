a15a0e8d5abddcb47973aacf651f808ceb0e15a3
asantoro36 <axelsantoro@hotmail.com>
2024-02-01 19:46:49 -0300

    Add signUp endpoint and controllers

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,11 @@ const app = express();
 
 paths = {
     status: "/ping",
+    loginV1: "/v1/auth"
 };
 
 app.use(paths.status, require("./routes/healthCheck"));
+app.use(paths.loginV1, require("./routes/auth"));
 
 
 const PORT = 8080;
