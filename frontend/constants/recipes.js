9a3ec566e7acb123a4ab0291463d321402482771
Federico <federico.bergantinos@kueski.com>
2024-02-11 18:35:24 -0300

    Init frontend

--- frontend/constants/recipes.js
+++ frontend/constants/recipes.js
@@ -1,124 +1,105 @@
 export default [
   {
     title: 'Ice cream is made with carrageenan …',
-    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
-    cta: 'Ver receta',
+    image: 'https://s3.abcstatics.com/media/gurmesevilla/2012/01/comida-rapida-casera.jpg',
     tags: [ 'vegetariana', 'rapida', 'vegana', 'celiacas', 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'Is makeup one ofsdh5rw5rtwrdsf your daily esse …',
-    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
-    cta: 'Ver receta',
+    image: 'https://static-cse.canva.com/blob/598703/Fotografiadecomida.jpg',
     tags: [ 'vegana', 'celiacas', 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'Coffee is more tfhshtsergfdgsdhan just a drink: It’s …',
-    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://www.recetasnestle.com.ar/sites/default/files/2022-06/ingredientes-comida-de-mar-parrilla.jpg',
     tags: [ 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'Fashion is a popularsdgsretwetwhstyle, especially in …',
-    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://www.elmueble.com/medio/2023/05/23/tacos_fcb4f631_00541381_230523124558_1500x2032.jpg',
     tags: ['carbohidratos', 'sodio', 'antiinflamatoria', 'flora']
   },
   {
     title: 'Yummly is a greatsdgsersaefree UI packag …',
-    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
-    cta: 'Ver receta',
+    image: 'https://cdn.aarp.net/content/dam/aarp/health/caregiving/2018/03/1140-nutrients-food-loved-ones-caregiving-esp.jpg',
     tags: ['carbohidratos', 'sodio']
   },
 
   {
     title: '1Ice cream is madsdgsrhrsee with carrageenan …',
-    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
-    cta: 'Ver receta',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkGTV9ptpoJ1nv8SE8QJ_A4-pCjnd46axWiA&usqp=CAU',
     tags: ['carbohidratos', 'sodio']
   },
   {
     title: '2Is makeup one osdgsdgewreef your daily esse …',
-    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
-    cta: 'Ver receta',
+    image: 'https://www.laylita.com/recetas/wp-content/uploads/2023/08/Receta-de-la-sopa-de-mani.jpg',
     tags: [ 'vegetariana', 'rapida', 'vegana', 'celiacas', 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: '3Coffee is more thasdgrwerweyn just a drink: It’s …',
-    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    image: 'https://img.freepik.com/foto-gratis/primer-plano-carne-asada-salsa-verduras-patatas-fritas-plato-sobre-mesa_181624-35847.jpg',
     cta: 'Ver receta'  ,
     tags: ['carbohidratos', 'sodio', 'antiinflamatoria', 'flora']
   },
   {
     title: '4Fashion is a pophsrtsdfgserular style, especially in …',
-    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
-    cta: 'Ver receta',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_wulETjl6TbYZ3Z4M088Gn3BDvEPhVmYiTQ&usqp=CAU',
     tags: [ 'inmune', 'antiinflamatoria', 'flora'] 
   },
   {
     title: '5Yummly sdgsfis a great free UI packag …',
-    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4FR8wrs9_0hB3XpxnrggBXRjt1D6F2cLOqQ&usqp=CAU',
     tags: ['carbohidratos', 'sodio', 'antiinflamatoria', 'flora']
   },
   {
     title: 'Ice csdfsdfream is made with carrageenan …',
-    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://www.recetasnestle.com.co/sites/default/files/inline-images/comidas-fritas-plato-apanado-ensalada.jpg',
     tags: ['carbohidratos', 'sodio', 'antiinflamatoria', 'flora']
   },
   {
     title: 'sefsgsIs makeup one of your daily esse …',
-    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
-    cta: 'Ver receta',
+    image: 'https://www.gastronomiaycia.com/wp-content/uploads/2021/01/elplatoharvard_2-680x448.jpg',
     tags: [ 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'sdggrCoffee is more than just a drink: It’s …',
-    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://img.europapress.es/fotoweb/fotonoticia_20180117114451_1200.jpg',
     tags: [ 'vegetariana', 'rapida', 'vegana', 'celiacas', 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'fgsfdFashion is a popular style, especially in …',
-    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrgUcf2pA82UbEWGZy53h2SblBfjuh7HbSidmopFmeV8vxEGZmZWSJZ1YmcxNZN_Hdz9M&usqp=CAU',
     tags: ['carbohidratos', 'sodio']
   },
   {
     title: 'sdfsdYummly is a great free UI packag …',
-    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
-    cta: 'Ver receta',
+    image: 'https://www.cocinacaserayfacil.net/wp-content/uploads/2020/04/Recetas-de-comidas-para-ni%C3%B1os.jpg',
     tags: [ 'inmune', 'antiinflamatoria', 'flora']
   },
 
   {
     title: '1sdgfweIce cream is made with carrageenan …',
-    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
-    cta: 'Ver receta',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTm7KZl79yp8Hh8WOP8aRcoTYzaf3_xpXxEA&usqp=CAU',
     tags: [ 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'qewadsfIs makeup one of your daily esse …',
-    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
-    cta: 'Ver receta',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSLWNBIqTtuhQjtMtQZ0JZYECLVb0f4JokSg&usqp=CAU',
     tags: [ 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: '3fsgewCoffee is more than just a drink: It’s …',
-    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://images.unsplash.com/photo-1539136788836-5699e78bfc75?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGxhdG8lMjBkZSUyMGNvbWlkYXxlbnwwfHwwfHx8MA%3D%3D',
     tags: [ 'vegetariana', 'rapida', 'vegana', 'celiacas', 'inmune', 'antiinflamatoria', 'flora']
   },
   {
     title: 'asdasdFashion is a popular style, especially in …',
-    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
-    cta: 'Ver receta' ,
+    image: 'https://cdn.pixabay.com/photo/2018/05/21/22/52/gourmet-food-3419926_1280.jpg',
     tags: ['carbohidratos', 'sodio']
   },
   {
     title: '5asdasdgYummly is a great free UI packag …',
-    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
-    cta: 'Ver receta',
+    image: 'https://aprende.com/wp-content/uploads/2021/12/clases-online-de-chef.jpg',
     tags: [ 'inmune', 'antiinflamatoria', 'flora']
   },
 ];
